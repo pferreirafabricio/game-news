6ccbae3cebec489641e06f4b6772573436e55260
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2020-10-05 20:57:07 -0300

    add headers configuration

--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -7,3 +7,13 @@ define('CONF_DB_HOST', 'localhost');
 define('CONF_DB_NAME', 'gamenews');
 define('CONF_DB_USER', 'root');
 define('CONF_DB_PASSWORD', '');
+
+/**
+ * API
+ */
+define('CONF_API_ALLOW_ORIGIN', '*');
+define('CONF_API_CONTENT_TYPE', 'application/json');
+define('CONF_API_CHARSET', 'utf-8');
+define('CONF_API_ALLOW_METHODS', ["GET", "POST", "PUT", "DELETE"]);
+define('CONF_API_MAX_AGE', '3600');
+define('CONF_API_ALLOW_HEADERS', ["Content-Type", "Access-Controll-Allow-Headers", "Authorization"]);
